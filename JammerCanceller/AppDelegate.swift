eb8a31b25476338f74353ba16522fdc18dbe3fa6
John Doe <johndoe@example.com>
2019-02-09 20:35:32 +0900

    Initial Commit

---
+++ JammerCanceller/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  JammerCanceller
+//
+//  Created by 金本翔太 on 2019/02/09.
+//  Copyright © 2019 金本翔太. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
