819c6fde98d4edc9d1f1b90b55e7d468744faab0
John Doe <johndoe@example.com>
2019-02-22 00:30:09 +0900

    Extension First Commit

---
+++ JammerCancellerExtension/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  JammerCancellerExtension
+//
+//  Created by 金本翔太 on 2019/02/09.
+//  Copyright © 2019 金本翔太. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
