10e27d0af9c6be535c14568876b43c34c6b908b8
John Doe <johndoe@example.com>
2019-03-12 12:30:49 +0900

    add remove row button

--- JammerCancellerExtension/View/UrlTableViewDelegate.swift
+++ JammerCancellerExtension/View/UrlTableViewDelegate.swift
@@ -20,9 +20,25 @@ class UrlTableViewDelegate : NSObject, NSTableViewDelegate {
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         NSLog("tableView -> NSView 行番号 : " + String(row))
-        if let cell = tableView.makeView(withIdentifier: CellID.Url, owner: nil) as? NSTableCellView {
-            cell.textField?.stringValue = self.dataSource.urlList[row]
-            return cell
+        if tableColumn == tableView.tableColumns[0] {
+            // url column
+            if let cell = tableView.makeView(withIdentifier: CellID.Url, owner: nil) as? NSTableCellView {
+                cell.textField?.stringValue = self.dataSource.urlList[row]
+                return cell
+            }
+        } else if tableColumn == tableView.tableColumns[1] {
+            // remove row button
+            if let cell = tableView.makeView(withIdentifier: CellID.Remove, owner: nil ) as? NSTableCellView {
+                NSLog("add remove button ----")
+                let rect = NSRect(x: 0, y: 0, width: 15, height: 15)
+                let button = NSButton(frame: rect)
+                button.setButtonType(NSButton.ButtonType.momentaryPushIn)
+                button.title = "-"
+                button.tag = row
+                cell.addSubview(button)
+                cell.textField?.stringValue = ""
+                return cell
+            }
         }
         return nil
     }
@@ -30,4 +46,5 @@ class UrlTableViewDelegate : NSObject, NSTableViewDelegate {
 
 fileprivate enum CellID {
     static let Url = NSUserInterfaceItemIdentifier("UrlCellID")
+    static let Remove = NSUserInterfaceItemIdentifier("RemoveCellID")
 }
